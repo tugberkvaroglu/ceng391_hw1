718adeb9e15c16957309000a6fc2f1a437d9b7d8
tugberk.varoglu <tugberkvaroglu03@gmail.com>
2024-07-18 00:43:52 +0300

    base

---
+++ build/CMakeFiles/image_test.dir/DependInfo.cmake
@@ -0,0 +1,20 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/home/tugberk/Desktop/ceng391_hw1/app/image_test.cpp" "CMakeFiles/image_test.dir/app/image_test.cpp.o" "gcc" "CMakeFiles/image_test.dir/app/image_test.cpp.o.d"
+  "/home/tugberk/Desktop/ceng391_hw1/src/image.cpp" "CMakeFiles/image_test.dir/src/image.cpp.o" "gcc" "CMakeFiles/image_test.dir/src/image.cpp.o.d"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ build/CMakeFiles/image_test.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/image_test.dir/app/image_test.cpp.o"
+  "CMakeFiles/image_test.dir/app/image_test.cpp.o.d"
+  "CMakeFiles/image_test.dir/src/image.cpp.o"
+  "CMakeFiles/image_test.dir/src/image.cpp.o.d"
+  "image_test"
+  "image_test.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/image_test.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
